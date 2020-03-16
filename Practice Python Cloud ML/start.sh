8aa60efb4e0d4de71873fdbb5a8244beb4a51317
EricC <Eric.Curley94@gmail.com>
2020-03-16 16:30:44 -0400

    Submit job to google cloud....

---
+++ Practice Python Cloud ML/start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+export BUCKET_NAME=erk-test-data-bucket
+export JOB_NAME="mnist_mlp_train_$(date +%Y%m%d_%H%M%S)"
+export JOB_DIR=gs://$BUCKET_NAME/$JOB_NAME
+export REGION=us-east1
+
+gcloud ml-engine jobs submit training $JOB_NAME \
+    --job-dir $JOB_DIR \
+    --runtime-version 1.0 \
+    --module-name trainer.mnist_mlp \
+    --package-path ./trainer \
+    --region $REGION \
+    -- \
+    --train-file gs://$BUCKET_NAME/data/mnist.pkl
+
